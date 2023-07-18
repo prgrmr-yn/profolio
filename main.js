193814b2a49f1bd28ec4bf8f8e004f766e018dab
Yatin Nagpal <nnega4428@gmail.com>
2023-07-18 14:38:53 +1000

    its working, need work on the form and need to replace the place holder images

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let tabContents = document.getElementsByClassName('tab-contents')
 
 
 function opentab(tabName) {
-  for (let tablink of tabLinks){
+  for (const tablink of tabLinks){
     tablink.classList.remove('active-link')
   }
   for (const tabContent of tabContents) {
@@ -13,3 +13,32 @@ function opentab(tabName) {
   event.currentTarget.classList.add("active-link")
   document.getElementById(tabName).classList.add("active-tab")
 }
+
+let sideMenu = document.getElementById('side-menu');
+
+function openMenu(){
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
+
+// Form
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw6hGm-IYpYdpKCNOJtxmvBfYzNAUSUZsgptJ2dpy409r9kUZynMAbphso0rVEqouZb/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg= document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.textContent='Message Sent Successfully';
+      setTimeout(() => {
+        msg.textContent='';
+      }, 5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+});
